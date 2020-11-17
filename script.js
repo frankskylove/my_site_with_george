1df93257c7b79f971f6f478807256bc7e10e37e3
lovecodeabc <georgew7788@gmail.com>
2020-11-16 19:20:27 -0500

    Update index.html, script.js, and one other file

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 var age = prompt("How old are you?")
+if(isNaN(age)){
+if(age > 50){
+alert("You've been around for a while!")
+}else{alert("Wow, that's really young!")
+}
+}
+else{
+alert("That's not a number!")
+}
+function runFor(){
+for(var i = 0; i<5; i++){
+$("#main").append("<img class='loopimage' src='assets/dog.jpg")
+}
+}
